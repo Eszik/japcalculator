ce4c5956c1c4d193e4a6633eba11d32e99e418af
Thomas Bianco <thomas.bianco@student-cs.fr>
2021-10-22 14:25:10 +0200

    modified parrot

--- functions/parrot.js
+++ functions/parrot.js
@@ -1,6 +1,10 @@
-exports.handler = async function(event, context) {
-  const requestBody = JSON.parse(event.body);
-  const message = requestBody.message;
+exports.handler = async function (event, context) {
+  if (event.httpMethod === "GET") {
+    return {
+      statusCode: 400,
+      body: "GET request disallowed",
+    };
+  }
 
   console.log(event);
 
@@ -8,7 +12,7 @@ exports.handler = async function(event, context) {
     const headers = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Access-Control-Allow-Methods": "POST, PUT, DELETE",
     };
     return {
       statusCode: 200, // <-- Must be 200 otherwise pre-flight call fails
@@ -16,6 +20,9 @@ exports.handler = async function(event, context) {
       body: "This was a preflight call!",
     };
   }
+
+  const requestBody = JSON.parse(event.body);
+  const message = requestBody.message;
 
   if (message) {
     return {
